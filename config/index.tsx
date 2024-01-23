3a1e79e9b734db5fc17183756b3ce841ad9b0eb1
jjustin-35 <jjusttin11@gmail.com>
2024-01-23 23:45:36 +0800

    update: orders slice

--- config/index.tsx
+++ config/index.tsx
@@ -1,6 +1,6 @@
 const config = {
     HOST: 'http://localhost:3000',
-    API_URL: ' https://hotel-service-center.onrender.com'
+    API_URL: 'https://hotel-service-center.onrender.com'
 }
 
 export default config;
---
+++ redux/slices/orders.tsx
@@ -0,0 +1,160 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import fetchApi from "@/helpers/apiHelper";
+import { apiMethod } from "@/constants/types/api";
+import config from "@/config";
+import apiPaths from "@/constants/apis";
+
+type UserInfoType = {
+  address: {
+    zipCode: number;
+    detail: string;
+  };
+  name: string;
+  phone: string;
+  email: string;
+};
+
+type SetOrdersDataType = {
+  roomId: string;
+  checkInDate: string;
+  checkOutDate: string;
+  peopleNum: number;
+  userInfo: UserInfoType;
+};
+
+type InitialStateType = {
+  orders: Record<string, any>[];
+  orderDetail: Record<string, any>;
+  errorMessage: string;
+};
+
+export const getOrders = createAsyncThunk(
+  "orders/getOrders",
+  async (_, { rejectWithValue }) => {
+    try {
+      const data = await fetchApi(
+        `${config.API_URL}${apiPaths.orders}`,
+        apiMethod.GET
+      );
+
+      if (data.status === true) {
+        return data.result;
+      }
+
+      return rejectWithValue(data.message);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+export const setOrders = createAsyncThunk(
+  "orders/setOrders",
+  async (data: SetOrdersDataType, { rejectWithValue }) => {
+    try {
+      const resp = await fetchApi(
+        `${config.API_URL}${apiPaths.orders}`,
+        apiMethod.POST,
+        data
+      );
+
+      if (resp.status === true) {
+        return resp.result;
+      }
+
+      return rejectWithValue(resp.message);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+export const getOrderDetail = createAsyncThunk(
+  "orders/getOrderDetail",
+  async (id: string, { rejectWithValue }) => {
+    try {
+      const data = await fetchApi(
+        `${config.API_URL}${apiPaths.orders}/${id}`,
+        apiMethod.GET
+      );
+
+      if (data.status === true) {
+        return data.result;
+      }
+
+      return rejectWithValue(data.message);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+export const deleteOrder = createAsyncThunk(
+  "orders/deleteOrder",
+  async (id: string, { rejectWithValue }) => {
+    try {
+      const data = await fetchApi(
+        `${config.API_URL}${apiPaths.orders}/${id}`,
+        apiMethod.DELETE
+      );
+
+      if (data.status === true) {
+        return data.result;
+      }
+
+      return rejectWithValue(data.message);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+const initialState: InitialStateType = {
+  orders: [],
+  orderDetail: {},
+  errorMessage: "",
+};
+
+const ordersSlice = createSlice({
+  name: "orders",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getOrders.fulfilled, (state, action) => {
+      state.orders = action.payload;
+      state.errorMessage = "";
+    });
+    builder.addCase(getOrders.rejected, (state, action) => {
+      state.errorMessage = action.payload as string;
+    });
+
+    builder.addCase(setOrders.fulfilled, (state, action) => {
+      state.orderDetail = action.payload;
+      state.errorMessage = "";
+    });
+    builder.addCase(setOrders.rejected, (state, action) => {
+      state.errorMessage = action.payload as string;
+    });
+
+    builder.addCase(getOrderDetail.fulfilled, (state, action) => {
+      state.orderDetail = action.payload;
+      state.errorMessage = "";
+    });
+    builder.addCase(getOrderDetail.rejected, (state, action) => {
+      state.errorMessage = action.payload as string;
+    });
+
+    builder.addCase(deleteOrder.fulfilled, (state, action) => {
+      state.orderDetail = action.payload;
+      state.orders = state.orders.filter(
+        (order) => order._id !== action.payload._id
+      );
+      state.errorMessage = "";
+    });
+    builder.addCase(deleteOrder.rejected, (state, action) => {
+      state.errorMessage = action.payload as string;
+    });
+  },
+});
+
+export default ordersSlice.reducer;
