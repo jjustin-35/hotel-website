417691d7969b062117102c480e4baeec61a70c9b
jjustin-35 <jjusttin11@gmail.com>
2024-01-23 19:41:35 +0800

    update: redux config and add apiHelper

---
+++ constants/types/api.tsx
@@ -0,0 +1,6 @@
+export enum apiMethod {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+}
---
+++ helpers/apiHelper.tsx
@@ -0,0 +1,35 @@
+import { stringify } from "querystring";
+import { apiMethod } from "@/constants/types/api";
+
+const fetchApi = async (
+  url: string,
+  method: apiMethod,
+  data?: Record<string, string>,
+  headers?: Record<string, string>
+) => {
+  const reqHeaders = {
+    "Content-Type": "application/json",
+    ...headers,
+  };
+
+  const body = (() => {
+    if (method === apiMethod.GET) {
+      return stringify(data);
+    }
+
+    return JSON.stringify(data);
+  })();
+
+  const reqUrl = method === apiMethod.GET ? `${url}?${body}` : url;
+
+  const res = await fetch(reqUrl, {
+    method,
+    headers: reqHeaders,
+    ...(method !== apiMethod.GET && { body }),
+  });
+  const result = await res.json();
+
+  return result;
+};
+
+export default fetchApi;
