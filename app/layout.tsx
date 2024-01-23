71e8c57e10e6af20284812ce4c7d4b16db29912b
jjustin-35 <jjusttin11@gmail.com>
2024-01-24 00:00:11 +0800

    fix: redux provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Provider } from "react-redux";
 import "../constants/styles/global.scss";
-import store from "@/config/configureStore";
+
+import ReduxProvider from "@/libs/reduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider store={store}>{children}</Provider>
+        <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
   );
