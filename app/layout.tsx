21e430aa6206395defe9bf9d1164d9eb2f95b24d
yoyo <a4253607@gmail.com>
2024-02-01 01:11:33 +0800

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/all.scss'
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer/Footer";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Navbar from "../components/Header";
-import Footer from "../components/Footer/Footer";
 import HomePage from '../pages/Home';
+
+
 export default function Home() {
   return <main>
-    <Navbar/>
-      <HomePage />
-    <Footer />
+      <HomePage />       
   </main>;
 }
