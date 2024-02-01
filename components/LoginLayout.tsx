937a4b8cf5f3579b6e36d3c206fb6f1158a619fb
yoyo <a4253607@gmail.com>
2024-02-02 00:51:14 +0800

    loginForm component

---
+++ components/LoginLayout.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactNode } from "react";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const LoginLayout: FC<LayoutProps> = ({ children }) => {
+    return (
+        <section className="bg-black" style={{paddingTop:'120px'}}>
+            <div className="row row-cols-lg-2 row-cols-1  g-0">
+                <div className="col d-none d-lg-flex">
+                    <img src='/images/桌機版/register.png' className='w-100 object-cover' alt="" />
+                </div>
+                <div className="col l_login">
+                    {children}
+                </div>
+            </div>
+
+        </section>)
+}
+
+export default LoginLayout
