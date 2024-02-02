b67b43ef50bdb23064461a13b4cb68c306115829
jjustin-35 <jjusttin11@gmail.com>
2024-02-03 01:15:24 +0800

    update: history item

--- components/User/History/index.tsx
+++ components/User/History/index.tsx
@@ -1,5 +1,20 @@
+import { Card } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import Item from "./item";
+import { RootState } from "@/config/configureStore";
+
 const History = () => {
-  return <></>;
+  const { orders } = useSelector((state: RootState) => state.orders);
+  return (
+    <Card className="padding-40 d-flex flex-column gap-40 bg-white rounded-3">
+      <Card.Title className="fs-4 fw-bold">訂單紀錄</Card.Title>
+      <div>
+        {orders.map((order) => (
+          <Item key={order._id} item={order} />
+        ))}
+      </div>
+    </Card>
+  );
 };
 
 export default History;
